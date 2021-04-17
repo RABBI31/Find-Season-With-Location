95494f24c582eec3005b95de899a993a9f750f78
Rabbi <alirabbi3112@gmail.com>
2021-04-17 22:58:46 +0600

    Class COmponent

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import reactDom from 'react-dom';
 import  ReactDom from 'react-dom'
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position)=> console.log(position),
-        (err)=> console.log(err)
-    )
-    
-    return (
-        <div>
-            <h1>Hello there</h1>
-        </div>
-    )
+
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {lat: null, errorMessage: '' };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=>{
+                this.setState({lat:position.coords.latitude})
+            },
+            (err)=> {
+                this.setState({errorMessage: err.message})
+            }
+        );
+    }
+
+
+
+    render()
+   
+    {
+        if(this.state.errorMessage && !this.state.lat)
+          return(
+              <div>
+                  Error: {this.state.errorMessage}
+              </div>
+          )
+        if(!this.state.errorMessage && this.state.lat)
+          return(
+              <div>
+                  Latitude : {this.state.lat}
+              </div>
+          )
+      
+          return(
+              <div>
+                 Loading
+              </div>
+          )
+    }
 }
+
+
+
 ReactDom.render(<App />,document.querySelector('#root'))
