ebb56c5f5b3be96290222458d3e106fad0546802
Rabbi <alirabbi3112@gmail.com>
2021-04-18 16:06:24 +0600

    Season Determine

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -2,22 +2,45 @@ import React from 'react'
 
 const getSeason = (lat,month)=>{
     if (month > 1 && month < 5 ){
-        return lat > 0 ? 'pre-monsoon/ Summer' : 'Winter'
+        return lat > 0 ? 'Summer' : 'Winter'
     }
     if(month > 4 && month < 10){
         return lat > 0 ? 'Rainy Season' : 'Winter'
     }
     else{
-        return lat > 0 ? 'Winter' : 'Pre-monsoosn/Summer'
+        return lat > 0 ? 'Winter' : 'Summer'
     }
-}
+} 
 
 const SeasonDisplay = (props)=>{
     const season = getSeason(props.lat, new Date().getMonth())
-    console.log(season)
+    const text = ()=>{
+        if(season === 'Summer'){
+            return "Let's drink rasna "
+        }
+        if(season === 'Winter'){
+            return 'Goto Komboler niche'
+        }
+        else{
+            return 'Chole asho Chatar niche'
+        }
+    }
+    const icon = ()=>{
+        if(season === 'Summer'){
+            return "sun"
+        }
+        if(season === 'Winter'){
+            return 'snowflake'
+        }
+        else{
+            return 'umbrella'
+        }
+    }
     return(
         <div>
-            <h1>SeasonDisplay</h1>
+            <i className = {`${icon} icon`} />
+            <h1>{text()}</h1>
+            <i className = {`${icon} icon`} />
         </div>
     )
 }
